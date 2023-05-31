8bd9b621eef8c61a2dcdd6406f669dce2a9b6233
pradnya-git-dev <kandarkarp@gmail.com>
2023-05-31 15:00:52 -0400

    chapter_1_strategy_pattern

---
+++ chapter1/src/Duck.java
@@ -0,0 +1,28 @@
+public abstract class Duck {
+    FlyBehaviour flyBehaviour;
+    QuackBehaviour quackBehaviour;
+
+    public Duck() {}
+
+    public abstract void display();
+
+    public void performFly() {
+        flyBehaviour.fly();
+    }
+
+    public void performQuack() {
+        quackBehaviour.quack();
+    }
+
+    public void swim() {
+        System.out.println("All ducks can swim, even the decoys!");
+    }
+
+    public void setFlyBehaviour(FlyBehaviour flyBehaviour) {
+        this.flyBehaviour = flyBehaviour;
+    }
+
+    public void setQuackBehaviour(QuackBehaviour quackBehaviour) {
+        this.quackBehaviour = quackBehaviour;
+    }
+}
---
+++ chapter1/src/MallardDuck.java
@@ -0,0 +1,12 @@
+public class MallardDuck extends Duck {
+
+    public MallardDuck() {
+        quackBehaviour = new Quack();
+        flyBehaviour = new FlyWithWings();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I'm a real mallard duck!");
+    }
+}
